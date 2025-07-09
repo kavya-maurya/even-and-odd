9d9e297bd0a4c1c6e9532242ca9018b3f1ab4d81
kavya <kavyamaurya269@gmail.com>
2025-07-09 13:41:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+namespace Program 
+{
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("to detect whether number is even or odd");
+        Console.WriteLine(" ______________________________________________");
+        Console.Write("| please provide the number you want to verify |: ");
+        
+        int number = Convert.ToInt32 (Console.ReadLine ( ));
+       
+        Console.WriteLine(" ----------------------------------------------");
+        Console.WriteLine("  ");
+
+        if ((number/2)*2 == number  )
+        {
+            Console.WriteLine($"{number} is even number");
+        }
+
+        else
+        {
+            Console.WriteLine ($"{number} is odd number");
+        }
+        
+        
+    }
+}
+
+}
